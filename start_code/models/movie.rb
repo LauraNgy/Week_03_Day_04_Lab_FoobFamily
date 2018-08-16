61ddbc53582e4143691d72f7a20e7646198df2eb
Laura Nagy <ngylaura@gmail.com>
2018-08-16 15:54:38 +0100

    adds movie.stars and star.movies methods

--- start_code/models/movie.rb
+++ start_code/models/movie.rb
@@ -1,78 +1,88 @@
 class Movie
 
-    attr_reader :id
-    attr_accessor :title, :genre, :budget
+  attr_reader :id
+  attr_accessor :title, :genre, :budget
 
-      def initialize(options)
-        @title = options['title']
-        @genre = options['genre']
-        @budget = options['budget'].to_i
-        @id = options['id'].to_i if options['id']
-      end
+  def initialize(options)
+    @title = options['title']
+    @genre = options['genre']
+    @budget = options['budget'].to_i
+    @id = options['id'].to_i if options['id']
+  end
 
-      def save()
-        sql = "
-          INSERT INTO movies
-          (title, genre, budget)
-          VALUES
-          ($1, $2, $3)
-          RETURNING *
-        "
-        values = [@title, @genre, @budget]
-        @id = SqlRunner.run(sql, values)[0]['id'].to_i
-      end
+  def save()
+    sql = "
+      INSERT INTO movies
+      (title, genre, budget)
+      VALUES
+      ($1, $2, $3)
+      RETURNING *
+    "
+    values = [@title, @genre, @budget]
+    @id = SqlRunner.run(sql, values)[0]['id'].to_i
+  end
 
-      def update()
-        sql = "
-          UPDATE
-            movies
-          SET
-            (title, genre, budget) = ($1, $2, $3)
-          WHERE
-            id = $4
-          "
-        values = [@title, @genre, @budget, @id]
-        SqlRunner.run(sql, values)
-      end
+  def update()
+    sql = "
+      UPDATE
+        movies
+      SET
+        (title, genre, budget) = ($1, $2, $3)
+      WHERE
+        id = $4
+      "
+    values = [@title, @genre, @budget, @id]
+    SqlRunner.run(sql, values)
+  end
 
-      def delete()
-        sql = "
-          DELETE FROM
-            movies
-          WHERE
-            id = $1
-          "
-        values = [@id]
-        SqlRunner.run(sql, values)
-      end
+  def delete()
+    sql = "
+      DELETE FROM
+        movies
+      WHERE
+        id = $1
+      "
+    values = [@id]
+    SqlRunner.run(sql, values)
+  end
 
-      # def movies()
-      #   sql = "
-      #     SELECT * FROM
-      #       movies
-      #       WHERE
-      #       id = $1
-      #     "
-      #   values = [@artistID]
-      #   result = SqlRunner.run(sql, values)
-      #   return result.map { |param| Artist.new(param) }[0]
-      # end
+  def stars()
+    sql = "
+      SELECT
+        stars.*
+      FROM
+        stars
+      INNER JOIN
+        castings
+      ON
+        castings.star_id = stars.id
+      WHERE
+        castings.movie_id = $1
+      "
+    values = [@id]
+    stars = SqlRunner.run(sql, values)
+    return Star.map_items(stars)
+  end
 
-      def Movie.all()
-        sql = "SELECT * FROM movies"
-        movies = SqlRunner.run(sql)
-        return self.map_items(movies)
-      end
+  def remaining_budget
 
-      def self.map_items(movie_data)
-        result = movie_data.map { |movie| Movie.new(movie) }
-        return result
-      end
+  end
 
-      def Movie.delete_all()
-        sql = "DELETE FROM movies"
-        SqlRunner.run(sql)
-      end
+  def Movie.all()
+    sql = "SELECT * FROM movies"
+    movies = SqlRunner.run(sql)
+    return self.map_items(movies)
+  end
+
+  def self.map_items(movie_data)
+    result = movie_data.map { |movie| Movie.new(movie) }
+    return result
+  end
+
+  def Movie.delete_all()
+    sql = "DELETE FROM movies"
+    SqlRunner.run(sql)
+  end
 
 
 end
--- start_code/models/star.rb
+++ start_code/models/star.rb
@@ -45,17 +45,23 @@ class Star
       SqlRunner.run(sql, values)
     end
 
-    # def movies()
-    #   sql = "
-    #     SELECT * FROM
-    #       stars
-    #       WHERE
-    #       id = $1
-    #     "
-    #   values = [@artistID]
-    #   result = SqlRunner.run(sql, values)
-    #   return result.map { |param| Artist.new(param) }[0]
-    # end
+    def movies()
+      sql = "
+        SELECT
+          movies.*
+        FROM
+          movies
+        INNER JOIN
+          castings
+        ON
+          castings.movie_id = movies.id
+        WHERE
+          castings.star_id = $1
+        "
+      values = [@id]
+      movies = SqlRunner.run(sql, values)
+      return Movie.map_items(movies)
+    end
 
     def Star.all()
       sql = "SELECT * FROM stars"
