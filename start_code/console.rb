2c58b5f21a80b1dbb69f617b2edc38d97344329e
Laura Nagy <ngylaura@gmail.com>
2018-08-16 15:26:35 +0100

    adds casting model

--- start_code/console.rb
+++ start_code/console.rb
@@ -6,21 +6,58 @@ require_relative('db/sql_runner')
 
 Star.delete_all
 Movie.delete_all
+Casting.delete_all
 
 star1 = Star.new({
   'first_name' => 'Amy',
   'last_name' => 'Foob'
   })
 
-  star1.save()
+star2 = Star.new({
+  'first_name' => 'Mindy',
+  'last_name' => 'Foob'
+  })
 
-  movie1 = Movie.new({
-    'title' => 'The Foob Sisters',
+star1.save()
+star2.save()
+
+movie1 = Movie.new({
+  'title' => 'The Foob Sisters',
+  'genre' => 'comedy',
+  'budget' => '50000000'
+  })
+
+  movie1.save()
+
+  movie2 = Movie.new({
+    'title' => 'Foob Life',
     'genre' => 'comedy',
-    'budget' => '50000000'
+    'budget' => '20000000'
     })
 
-    movie1.save()
+    movie2.save()
+
+casting1 = Casting.new({
+  'movie_id' => movie1.id,
+  'star_id' => star1.id,
+  'fee' => 10000000
+  })
+casting2 = Casting.new({
+  'movie_id' => movie1.id,
+  'star_id' => star2.id,
+  'fee' => 10000005
+  })
+
+  casting3 = Casting.new({
+    'movie_id' => movie2.id,
+    'star_id' => star2.id,
+    'fee' => 5000005
+    })
+
+
+casting1.save()
+casting2.save()
+casting3.save()
 
   binding.pry
 
--- start_code/models/casting.rb
+++ start_code/models/casting.rb
@@ -1,3 +1,78 @@
 class Casting
 
+    attr_reader :id
+    attr_accessor :movie_id, :star_id, :fee
+
+      def initialize(options)
+        @movie_id = options['movie_id']
+        @star_id = options['star_id']
+        @fee = options['fee'].to_i
+        @id = options['id'].to_i if options['id']
+      end
+
+      def save()
+        sql = "
+          INSERT INTO castings
+          (movie_id, star_id, fee)
+          VALUES
+          ($1, $2, $3)
+          RETURNING *
+        "
+        values = [@movie_id, @star_id, @fee]
+        @id = SqlRunner.run(sql, values)[0]['id'].to_i
+      end
+
+      def update()
+        sql = "
+          UPDATE
+            castings
+          SET
+            (movie_id, star_id, fee) = ($1, $2, $3)
+          WHERE
+            id = $4
+          "
+        values = [@movie_id, @star_id, @fee, @id]
+        SqlRunner.run(sql, values)
+      end
+
+      def delete()
+        sql = "
+          DELETE FROM
+            castings
+          WHERE
+            id = $1
+          "
+        values = [@id]
+        SqlRunner.run(sql, values)
+      end
+
+      # def movies()
+      #   sql = "
+      #     SELECT * FROM
+      #       movies
+      #       WHERE
+      #       id = $1
+      #     "
+      #   values = [@artistID]
+      #   result = SqlRunner.run(sql, values)
+      #   return result.map { |param| Artist.new(param) }[0]
+      # end
+
+      def Casting.all()
+        sql = "SELECT * FROM castings"
+        castings = SqlRunner.run(sql)
+        return self.map_items(castings)
+      end
+
+      def self.map_items(casting_data)
+        result = casting_data.map { |casting| Casting.new(casting) }
+        return result
+      end
+
+      def Casting.delete_all()
+        sql = "DELETE FROM castings"
+        SqlRunner.run(sql)
+      end
+
+
 end
